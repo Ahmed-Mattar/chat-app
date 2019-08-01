deb93f52668be23f9656917130b85acff9f881a9
Ahmed Mattar <AceMattar@gmail.com>
2019-08-01 23:41:24 +0200

    feat: basic Tracking users joining and leaving

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 const {generateMessage, generateLocationMessage} = require('./utils/messages')
-
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -17,11 +17,19 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
     
-    socket.on('join', ({username, room}) => {
-        socket.join(room)
+    socket.on('join', ({username, room}, callback) => {
+        const {error, user} = addUser({ id:socket.id, username, room })
+
+        if (error) {
+           return callback(error)
+        }
+
+        socket.join(user.room)
 
         socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined`))
+
+        callback()
     })
 
 
@@ -38,7 +46,12 @@ io.on('connection', (socket) => {
 
     
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+        }
+
     })
 
     socket.on('sendLocation', ({latitude,longitude}, callback) => {
